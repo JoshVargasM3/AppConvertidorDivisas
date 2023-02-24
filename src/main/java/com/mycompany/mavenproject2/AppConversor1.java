50980862b3eb6a6cf51c60ea3c5d75447d90a32c
Ing. José Luis Vargas Ríos <112647852+JoshVargasM3@users.noreply.github.com>
2023-02-24 17:22:29 -0600

    New files App Java ConversorDivisas

---
+++ src/main/java/com/mycompany/mavenproject2/AppConversor1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author JoshV
+ */
+public class AppConversor1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
